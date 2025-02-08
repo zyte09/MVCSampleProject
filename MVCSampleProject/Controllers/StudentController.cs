f4f375c7454f0a00af5e37b9b84c975b00bdda7f
zyte09 <allen.aranez32@gmail.com>
2025-02-08 13:28:45 +0800

    Add project files.

---
+++ MVCSampleProject/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCSampleProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Student_Info()
+        {
+            return View();
+        }
+
+    }
+}
